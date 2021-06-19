0e7855783515eee35e51f20af32056633a9b296d
Sandra01l <84776275+Sandra01l@users.noreply.github.com>
2021-06-19 14:55:30 +0530

    Update create.sql

--- create.sql
+++ create.sql
@@ -1,24 +1,25 @@
-create table publisher(
-        id integer primary key, 
+-- create a table
+CREATE TABLE publisher(
+        id integer PRIMARY KEY, 
         name text,
         country text
         );
 
-create table books(
-        id integer primary key,
+CREATE TABLE books(
+        id integer PRIMARY KEY,
         title text,
         publisher integer,
-        foreign key (publisher) references publisher(id)
+        FOREIGN KEY (publisher) REFERENCES publisher(id)
         );
-        
-create table subjects(
-        id integer primary key,
+
+CREATE TABLE subjects(
+        id integer PRIMARY KEY,
         name text 
         );
 
-create table books_subjects(
+CREATE TABLE books_subjects(
         book integer,
         subject integer,
-        foreign key (book) references books(id),
-        foreign key (subject) references subjects(id)
+        FOREIGN KEY (book) REFERENCES books(id),
+        FOREIGN KEY (subject) REFERENCES subjects(id)
         ); 
