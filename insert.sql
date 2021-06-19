9c7382da6bb9c7e87b98d9afaa491be69816e7ec
Sandra01l <84776275+Sandra01l@users.noreply.github.com>
2021-06-19 15:06:43 +0530

    Update insert.sql

--- insert.sql
+++ insert.sql
@@ -1,47 +1,50 @@
-INSERT INTO publisher VALUES
-        (1,'PHI','India'),
-        (2,'Harper','USA'),
-        (3,'GCP','USA'),
-        (4,'Avery','USA'),
-        (5,'Del Rey','UK'),
-        (6,'Vintage','UK');
+INSERT INTO publisher(name, country) VALUES("PHI", "India");
+INSERT INTO publisher(name, country) VALUES("Harper", "USA");
+INSERT INTO publisher(name, country) VALUES("GCP", "USA");
+INSERT INTO publisher(name, country) VALUES("Avery", "USA");
+INSERT INTO publisher(name, country) VALUES("Del Rey", "UK");
+INSERT INTO publisher(name, country) VALUES("Vintage", "UK");
 
-INSERT INTO books VALUES
-        (1,'The C Programming Language',1),
-        (2,'The Go Programming Language',1),
-        (3,'The UNIX Programming Environment',1),
-        (4,'Cryptonomicon',2),
-        (5,'Deep Work',3),
-        (6,'Atomic Habits',4),
-        (7,'The City and The City',5),
-        (8,'The Great War for Civilisation',6);
+INSERT INTO books(title, publisher) VALUES("The C Programming Language", 1);
+INSERT INTO books(title, publisher) VALUES("The Go Programming Language", 1);
+INSERT INTO books(title, publisher) VALUES("The UNIX Programming Environment", 1);
+INSERT INTO books(title, publisher) VALUES("Cryptonomicon", 2);
+INSERT INTO books(title, publisher) VALUES("Deep Work", 3);
+INSERT INTO books(title, publisher) VALUES("Atomic Habits", 4);
+INSERT INTO books(title, publisher) VALUES("The City and The City", 5);
+INSERT INTO books(title, publisher) VALUES("The Great War for Civilisation", 6);
 
-INSERT INTO subjects VALUES 
-        (1, 'C'),
-        (2, 'UNIX'),
-        (3, 'Technology'),
-        (4, 'Go'),
-        (5,'Science Fiction'),
-        (6,'Productivity'),
-        (7,'Psychology'),
-        (8, 'Politics'),
-        (9, 'History');
+INSERT INTO subjects(name) VALUES("C");
+INSERT INTO subjects(name) VALUES("UNIX");
+INSERT INTO subjects(name) VALUES("Technology");
+INSERT INTO subjects(name) VALUES("Go");
+INSERT INTO subjects(name) VALUES("Science Fiction");
+INSERT INTO subjects(name) VALUES("Productivity");
+INSERT INTO subjects(name) VALUES("Psychology");
+INSERT INTO subjects(name) VALUES("Politics");
+INSERT INTO subjects(name) VALUES("History");
 
-INSERT INTO books_subjects VALUES 
-        (1,1),
-        (1,2),
-        (1,3),
-        (2,3),
-        (2,4),
-        (3,2),
-        (3,3),
-        (4,3),
-        (4,5),
-        (5,3),
-        (5,6),
-        (6,6),
-        (6,7),
-        (7,5),
-        (7,8),
-        (8,8),
-        (8,9);
+INSERT INTO books_subjects(book, subject) VALUES(1, 1);
+INSERT INTO books_subjects(book, subject) VALUES(1, 2);
+INSERT INTO books_subjects(book, subject) VALUES(1, 3);
+
+INSERT INTO books_subjects(book, subject) VALUES(2, 3);
+INSERT INTO books_subjects(book, subject) VALUES(2, 4);
+
+INSERT INTO books_subjects(book, subject) VALUES(3, 2);
+INSERT INTO books_subjects(book, subject) VALUES(3, 3);
+
+INSERT INTO books_subjects(book, subject) VALUES(4, 3);
+INSERT INTO books_subjects(book, subject) VALUES(4, 5);
+
+INSERT INTO books_subjects(book, subject) VALUES(5, 3);
+INSERT INTO books_subjects(book, subject) VALUES(5, 6);
+
+INSERT INTO books_subjects(book, subject) VALUES(6, 6);
+INSERT INTO books_subjects(book, subject) VALUES(6, 7);
+
+INSERT INTO books_subjects(book, subject) VALUES(7, 5);
+INSERT INTO books_subjects(book, subject) VALUES(7, 8;
+                                                 
+INSERT INTO books_subjects(book, subject) VALUES(8, 8);
+INSERT INTO books_subjects(book, subject) VALUES(8, 9);
