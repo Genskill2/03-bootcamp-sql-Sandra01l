8b2521bfdffdb91b3a45c3ff54b53ce531ed8cb8
Sandra01l <84776275+Sandra01l@users.noreply.github.com>
2021-06-19 15:32:35 +0530

    Update delete1.sql

--- delete1.sql
+++ delete1.sql
@@ -1,2 +1,2 @@
-delete from books_subjects where books_subjects.subject = ( select id from subjects where name = 'History');
-delete from subjects where name = 'History';
+delete from books_subjects where books_subjects.subject=(select id from subjects where name='History');
+delete from subjects where name='History';
